6b530ef3ab88b2de949c044cd665bef62cf9a4cd
Cooper55555 <milanvelthaak1@gmail.com>
2024-11-13 07:38:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+function next() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "block";
+    document.getElementById("card5").style.display = "block";
+    document.getElementById("card6").style.display = "block";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "flex";
+    document.getElementById("arrowbuttons2").style.display = "none";
+}
+
+function previous1() {
+    document.getElementById("card1").style.display = "block";
+    document.getElementById("card2").style.display = "block";
+    document.getElementById("card3").style.display = "block";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "flex";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+}
+
+function next1() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "block";
+    document.getElementById("card8").style.display = "block";
+    document.getElementById("card9").style.display = "block";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "flex";
+}
+
+function previous2() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "block";
+    document.getElementById("card5").style.display = "block";
+    document.getElementById("card6").style.display = "block";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "flex";
+    document.getElementById("arrowbuttons2").style.display = "none";
+}
